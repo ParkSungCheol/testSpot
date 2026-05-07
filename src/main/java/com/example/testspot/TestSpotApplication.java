d0f6cfd9cb4d65f41916ace2b03cc04ae30e2054
ParkSungCheol <93702296+ParkSungCheol@users.noreply.github.com>
2026-05-07 16:44:35 +0900

    Add Spring Boot main application class

---
+++ src/main/java/com/example/testspot/TestSpotApplication.java
@@ -0,0 +1,13 @@
+package com.example.testspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpotApplication.class, args);
+    }
+
+}
